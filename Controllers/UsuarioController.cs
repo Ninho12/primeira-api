6087b54f2b4b6b835f6736b3d0718cd4b831a820
João Paulo Sena Padilha <joaopaulojpsp@gmail.com>
2022-09-17 20:27:36 -0300

    Primeiro Commit

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace primeira_api.Controllers
+{
+    public class UsuarioController
+    {
+        
+    }
+}
