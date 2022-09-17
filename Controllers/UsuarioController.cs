59b8eaf3403d70227e21f1cf479e0e854221df18
João Paulo Sena Padilha <joaopaulojpsp@gmail.com>
2022-09-17 20:46:46 -0300

    Adição do UsuarioController

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -2,11 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace primeira_api.Controllers
 {
-    public class UsuarioController
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
     {
-        
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora() {
+
+            var obj = new {
+
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+
+            };
+
+            return Ok(obj);
+        }
+
+
     }
 }
